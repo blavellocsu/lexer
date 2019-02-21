50c634de8ed499dd0c262cd29cd8d37fa8ffd6f8
Brandon Lavello <brandonlavello@csu.fullerton.edu>
2019-02-20 16:18:05 -0800

    Added file handling

--- lexer.cpp
+++ lexer.cpp
@@ -1,11 +1,53 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-#include <vector>
-#include <list>
-
+//#include <vector>
+//#include <list>
 using namespace std;
 
-int main(int argc, const char * argv[]) {
+//Global data
 
+const string keyword[] = { "int", "float", "bool", "if", "else", "then", "do", "while",
+                       "whileend", "do", "doend", "for", "and", "or", "function"};
+
+const string separators[] = {"'", "(", ")", "{", "}", "[", "]", ",", ".", ":", ";", "!"};
+
+const string operators[] = {"*", "+", "-", "=", "/", ">", "<", "%"};
+
+int main( int argc, const char * argv[] ) {
+
+  //check for command line arguments
+
+    string usage = "Usage: \"./<name> <filename.txt>\"";
+    //check for correct usage: "./lexer sample.txt"
+    if (argc != 2) {
+      cout << "Invalid Arguments.\n" << usage << endl;
+      exit(1);
+    }
+
+  //File handling
+
+    //read filename in from argument
+    string filename = argv[1];
+    //Create filesream
+    ifstream file(filename);
+    //check to see if file opened correctly
+    if (file.is_open()) {
+      cout << "File opened successfully." << endl;
+    } else {
+     cout << "File Error: Could not open file.\n" << usage << endl;
+     exit(1);
+    }
+
+  //Continue on...
+
+
+
+
+
+
+
+  //Test Print
+  cout << "End Program\n";
+  return 0;
 } //end main
