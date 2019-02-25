7ad7ff122c08bc960f13516f883d946809690eda
vdeaugustine <32307897+vdeaugustine@users.noreply.github.com>
2019-02-24 21:55:12 -0800

    Using String now instead of List to hold tokens
    Was able to successfully change it from a list to a string. Previously, we were only reading non-white spaces. Now the string contains white spaces. This will make breaking up the lexemes easier

--- lexer.cpp
+++ lexer.cpp
@@ -13,99 +13,106 @@ using namespace std;
 
 //Global data
 const string keyword[] = { "int", "float", "bool", "if", "else", "then", "do", "while",
-                       "whileend", "do", "doend", "for", "and", "or", "function"};
+    "whileend", "do", "doend", "for", "and", "or", "function"};
 const string separators[] = {"'", "(", ")", "{", "}", "[", "]", ",", ".", ":", ";", "!"};
 const string operators[] = {"*", "+", "-", "=", "/", ">", "<", "%"};
 
 bool isKeyword();
 bool isOperator();
-void removeComments(list<char> *l);
-void testPrint(list<char> *l);
+void removeComments(string *l);
+void testPrint(string *l);
 //string grabLexeme (list<char> *l);
 bool isAlpha (char);
 
 
 int main( int argc, const char * argv[] ) {
-  //check for command line arguments
+    //check for command line arguments
     string usage = "Usage: \"./<name> <filename.txt>\"";
     //check for correct usage: "./lexer sample.txt"
     if (argc != 2) {
-      cout << "Invalid Arguments.\n" << usage << endl;
-      exit(1);
+        cout << "Invalid Arguments.\n" << usage << endl;
+        exit(1);
     }
-
-  //File handling
+    
+    //File handling
     //read filename in from command line argument
     //see top of file for correct command line usage
     string filename = argv[1];
     char currentChar;
-    list <char> charList;
+    string charString;
     //Create filesream
     ifstream file(filename);
     //check to see if file opened correctly
     if (file.is_open()) {
-      cout << "File opened successfully." << endl;
+        cout << "File opened successfully." << endl;
         
-      // read content of txt file (including white spaces) into a string
-        string contents((std::istreambuf_iterator<char>(file)),
-                        std::istreambuf_iterator<char>());
+        // read content of txt file (including white spaces) into a string
+        string contents((istreambuf_iterator<char>(file)), istreambuf_iterator<char>());
         
         for (int i = 0; i < contents.length(); i++) {
-            charList.push_back(contents[i]);
+            charString.push_back(contents[i]);
         }
-
-
+        
+        
     } else {
-     cout << "File Error: Could not open file.\n" << usage << endl;
-     exit(1);
+        cout << "File Error: Could not open file.\n" << usage << endl;
+        exit(1);
     }
-
+    
     //remove comments from code
-    removeComments(&charList);
-
+    removeComments(&charString);
+    
     //spacing
     cout << "\n\n";
-
+    
     //output to see if comments are gone
-    testPrint(&charList);
-
-  //Test Print to see if it completed
-  cout << "\nEnd Program\n";
-
-  return 0;
+    testPrint(&charString);
+    
+    //Test Print to see if it completed
+    cout << "\nEnd Program\n";
+    
+    return 0;
 } //end main
 
 
 //This function accepts an address to a std::list<char>
 //It iterates through the list and deletes the commented phrases
-void removeComments(list<char> *l) {
-  //Remove Comments from charList
-  //iterate through list
-  for (newIterator itr = l->begin();
-   itr != l->end(); advance(itr,1)) {
-    //check if current char is equal to '!'
-    if (*itr == '!') {
-      //delete until find next '!'
-      do {
-        //charList.erase returns next element
-        //set itr = to list.erase
-        itr = l->erase(itr++);
-      } while (*itr != '!');
-      //jumps out of do loop at last '!', so delete last one
-      itr = l->erase(itr++);
-    }//end if '!'
-  }//end for
+void removeComments(string *l) {
+    //Remove Comments from charString
+    //iterate through string
+    // string length is going to change so we have to make it dynamic rather than putting into for loop directly
+    int len = l->length();
+    for (int i = 0; i < len; i++) {
+        //check if current char is equal to '!'
+        if (l->at(i) == '!') {
+            //mark where ! starts in the string index
+            int whereCommStart = i;
+            //don't want i to change because we need to go back to there after we delete
+            int j = i; // this will be where we are in the comments section
+            j++;
+            while (l->at(j) != '!'){
+                j++;
+            }
+            // if we got here, that means we found the next !
+            int whereCommEnd = j;
+            
+            // now we erase the substring between our two markers
+            l->erase(whereCommStart, whereCommEnd-whereCommStart+1);
+            len = l->length();
+        }//end if '!'
+        
+    }//end for
 }//end function
 
 //Accepts an address to a std::list<chars>
 //prints out list to console
-void testPrint (list<char> *l) {
-  //Print Function
+void testPrint (string *l) {
+    //Print Function
     
     cout << "This is the char list: ";
-  for (newIterator itr = l->begin(); itr != l->end(); ++itr) {
-    std::cout /*<< ' '*/ << *itr;
-  }
+    for (int i = 0; i < l->length(); i++) {
+        std::cout /*<< ' '*/ << l->at(i);
+    }
 }
 
 //string grabLexeme (list<char> *l) {
